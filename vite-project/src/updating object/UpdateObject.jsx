ef9cc12463f1f320a0f67529e1b3a157fa4ae054
AhsanInkiad <ahsaninkiadakib380082@gmail.com>
2025-01-08 01:16:46 +0600

    UpdateObject now have both right and wrong way of updating object.

--- vite-project/src/updating object/UpdateObject.jsx
+++ vite-project/src/updating object/UpdateObject.jsx
@@ -3,32 +3,51 @@ import { useState } from "react"
 export default function UpdateObject() {
     const [data, setData] = useState(
         {
-            name: "Tasin",
+            name: "John",
             age: "30",
             number: "69",
-            address:"asdasd"
+            address: "asdasd"
         }
     )
 
-    const [counter, setCounter] = useState(0)
+    const [data2, setData2] = useState(
+        {
+            name: "John",
+            age: "30",
+            number: "69",
+            address: "asdasd"
+        }
+    )
 
-    const updateName = () =>{
-        
+
+    const updateName = () => {
         setData({
             ...data,
-            name: 'Akib',
+            name: 'Diego',
         })
-       
+
+    }
+
+    const updateName2 = () => {
+        data2.name = "Diego"
+        setData2(data2)
+
     }
 
     return (
-        <div className="border my-10 p-10 flex justify-center">
+        <div className="border gap-10 my-10 p-10 flex justify-center">
             <div>
                 <p>Name: {data.name}</p>
+                <button className="border px-3 bg-slate-300 rounded-lg" onClick={updateName}>Change Name</button>
                 <p>Age:{data.age}</p>
                 <p>Number:{data.number}</p>
-                <button className="border px-3 bg-slate-300 rounded-lg" onClick={updateName}>Change Name</button>
-                <p>Counter: {counter}</p>
+            </div>
+
+            <div>
+                <p>Name: {data2.name}</p>
+                <button className="border px-3 bg-slate-300 rounded-lg" onClick={updateName2}>Change Name</button>
+                <p>Age:{data2.age}</p>
+                <p>Number:{data2.number}</p>
             </div>
         </div>
     )
